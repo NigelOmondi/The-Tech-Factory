5630009c20787ba5c15ff93db76b8ba5246b2c00
NigelOmondi <nigelomondi62@gmail.com>
2024-08-27 20:30:39 +0300

    Project setup

---
+++ app/(public)/index.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Public Entry Page</Text>
+    </View>
+  )
+}
+
+export default Index
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
